46e1e756d93ebf7015422cdf6500bd82b4299d78
João Nunes Rigo <joao_rigo@outlook.com>
2025-03-04 11:31:05 -0300

    Remove ExternalApiConfig do versionamento e adiciona ao .gitignore"

--- StocksAdmin.Api/ExternalApiConfig.cs
+++
@@ -1,7 +0,0 @@
-﻿public static class ExternalApiConfig
-{
-    public static string BaseUrl { get; } = "https://brapi.dev/api/quote/";
-    public static string UrlToken { get; } = "2LSJuFf7JLjMwi3oEqdhxX";
-
-    public static string SecretJWT = "139e6558390baa77d7ff30d75061139883f736dfb4626261b4a44d3d111cf627";
-}
